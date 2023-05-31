bd213516de86038db8a9cff14207bda0b084bd78
Babar Ali <62939512+hearthackerBabar@users.noreply.github.com>
2023-05-30 23:53:48 -0700

    Update fht.py

--- fht.py
+++ fht.py
@@ -1,69 +1,64 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/usr/bin/env python2
 
 # Navbar for Termux Tool
 
 # Print the navbar
-print_navbar() {
-    echo "==============================="
-    echo "         Facebook Tool         "
-    echo "==============================="
-    echo
-}
+def print_navbar():
+    print("===============================")
+    print("         Facebook Tool         ")
+    print("===============================")
+    print
 
 # Menu
-menu() {
-    clear
-    print_navbar
-    echo "1. Extract user's public information"
-    echo "2. Download user's profile picture"
-    echo "3. Send friend request"
-    echo "4. Exit"
-    echo
-    read -p "Enter your choice: " choice
-    case $choice in
-        1) extract_info ;;
-        2) download_profile_picture ;;
-        3) send_friend_request ;;
-        4) exit ;;
-        *) echo "Invalid choice. Please try again." ; menu ;;
-    esac
-}
+def menu():
+    clear()
+    print_navbar()
+    print("1. Extract user's public information")
+    print("2. Download user's profile picture")
+    print("3. Send friend request")
+    print("4. Exit")
+    print
+    choice = raw_input("Enter your choice: ")
+    if choice == '1':
+        extract_info()
+    elif choice == '2':
+        download_profile_picture()
+    elif choice == '3':
+        send_friend_request()
+    elif choice == '4':
+        exit()
+    else:
+        print("Invalid choice. Please try again.")
+        menu()
 
 # Get Facebook account link from the user
-get_account_link() {
-    clear
-    print_navbar
-    echo "Paste the user's Facebook account link:"
-    read -p "Link: " account_link
-    echo
-    echo "Enter the user's name:"
-    read -p "Name: " user_name
-    echo
-    echo "Enter the user's ID number:"
-    read -p "ID Number: " user_id
-    echo
-    echo "Account link: $account_link"
-    echo "Name: $user_name"
-    echo "ID Number: $user_id"
-    echo
-    read -p "Press Enter to continue..." enter_key
-    menu
-}
+def get_account_link():
+    clear()
+    print_navbar()
+    account_link = raw_input("Paste the user's Facebook account link: ")
+    print
+    user_name = raw_input("Enter the user's name: ")
+    print
+    user_id = raw_input("Enter the user's ID number: ")
+    print
+    print("Account link: " + account_link)
+    print("Name: " + user_name)
+    print("ID Number: " + user_id)
+    print
+    raw_input("Press Enter to continue...")
+    menu()
 
 # Extract user's public information
-extract_info() {
-    get_account_link
-}
+def extract_info():
+    get_account_link()
 
 # Download user's profile picture
-download_profile_picture() {
-    get_account_link
-}
+def download_profile_picture():
+    get_account_link()
 
 # Send friend request
-send_friend_request() {
-    get_account_link
-}
+def send_friend_request():
+    get_account_link()
 
-# Main executio
-menu
+# Main execution
+menu()
